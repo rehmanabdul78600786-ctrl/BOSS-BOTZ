549fd2c1640c2002902f1584cc191b12bd5e20bd
rehmanabdul78600786-ctrl <rehmanabdul78600786@gmail.com>
2025-11-19 18:16:44 +0500

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -40,7 +40,7 @@ cmd({
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363348739987203@newsletter',
-                newsletterName: config.OWNER_NAME,
+                newsletterName: config.✿.｡Ɓ𝗼𐍃𐍃,
                 serverMessageId: 143
             }
         };
