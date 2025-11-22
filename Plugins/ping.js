6981f6ec6dd3012280066060d81923d0e1c11b75
rehmanabdul78600786-ctrl <rehmanabdul78600786@gmail.com>
2025-11-22 16:34:29 +0500

    Update ping.js

--- Plugins/ping.js
+++ Plugins/ping.js
@@ -41,8 +41,8 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: "𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃",
+                    newsletterJid: '120363405061777123@newsletter',
+                    newsletterName: "×º𝓑𝖔𝙨𝙨º×",
                     serverMessageId: 143
                 }
             }
